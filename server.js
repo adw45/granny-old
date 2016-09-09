647374a618549db9a4493a4af696191d6c520670
Andy Weber <andrew.weber@opower.com>
2016-09-09 09:54:54 -0400

    removed old packages

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var io = require('socket.io');
-var http = require('http');
 var app = express();
 
 app.use(express.static('client'));
 app.use('/scripts', express.static( __dirname + '/node_modules'));
 
-var webServer = http.createServer(app).listen(8000);
-var socketServer = io.listen(webServer);
+app.listen(8000);
